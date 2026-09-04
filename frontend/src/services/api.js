d22a51668cfde7c1ba168157f8c7576a5145428f
Santanu Halder <dishahalder18@gmail.com>
2026-09-04 22:01:45 +0530

    Use Render API in production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://ecommerce-platform-tndy.onrender.com/api",
+  baseURL: "https://ecommerce-platform-tndy.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
